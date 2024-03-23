a5ab532be09d6a94ff3c48d479472cec68d0e06c
abolfazl <email@abolfazls.ir>
2024-03-23 11:26:35 +0330

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-          port: '',
-          pathname: '/seed/**',
-        },
-      ],
-    },};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/seed/**",
+      },
+    ],
+  },
+  output: "export",
+  basepath: "/Nextjs_Carousel_front_consept",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
