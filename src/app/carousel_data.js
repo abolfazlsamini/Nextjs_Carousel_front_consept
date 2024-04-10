c4eeada3e45d54013a5b937ac53d891577e8d027
Abolfazl <53626987+abolfazlsamini@users.noreply.github.com>
2024-04-10 12:06:56 +0330

    Update carousel_data.js

--- src/app/carousel_data.js
+++ src/app/carousel_data.js
@@ -1,6 +1,6 @@
 export const slides = [
   {
-    src: `/Nextjs_Carousel_front_consept/1.jpg`,
+    src: `1.jpg`,
     alt: "pic1",
     discription: "Get the Sphinx Tattoo Set for free.",
     link: "https://picsum.photos/seed/314{Math.random()}/1080/900",
@@ -8,7 +8,7 @@ export const slides = [
     key: "0",
   },
   {
-    src: `/Nextjs_Carousel_front_consept/2.jpg`,
+    src: `2.jpg`,
     alt: "pic2",
     discription: "Be ready to rock the stage with new Rewards!",
     link: "https://picsum.photos/seed/311{Math.random()}/1080/900",
@@ -16,7 +16,7 @@ export const slides = [
     key: "1",
   },
   {
-    src: `/Nextjs_Carousel_front_consept/3.jpg`,
+    src: `3.jpg`,
     alt: "pic3",
     discription:
       "Join Mario and The Rabbids on their galactic journey with new Rewards!",
@@ -25,7 +25,7 @@ export const slides = [
     key: "2",
   },
   {
-    src: `/Nextjs_Carousel_front_consept/4.jpg`,
+    src: `4.jpg`,
     alt: "pic4",
     discription: "Celebrate the Season 3 with new Rewards!",
     link: "https://picsum.photos/seed/3Math.random()}/200/120",
